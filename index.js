0f184eca7062c27913b4d9dcc08ea51fbe66d2ae
lidusol <37696987+lidusol@users.noreply.github.com>
2022-12-19 16:44:43 +0300

    added funtions

--- index.js
+++ index.js
@@ -79,3 +79,59 @@ function fizzBuzz() {
 }
 
 console.log(fizzBuzz());
+
+// Longest word
+function longestWord(sen) {
+  const wordArr = sen.toLowerCase().match(/[a-z0-9]+/g);
+  const sorted = wordArr.sort((a, b) => b.length - a.length);
+
+  const longestWordArr = sorted.filter(
+    (word) => word.length === sorted[0].length
+  );
+
+  return longestWordArr.length === 1 ? longestWordArr[0] : longestWordArr;
+}
+
+const testLongestWord1 = 'Hi, I am Lydia.';
+const testLongestWord2 = "Hello there, it's Lydia here.";
+console.log(longestWord(testLongestWord2));
+
+// Chunk array
+function chunkArray(arr, len) {
+  const chunkedArr = [];
+
+  let i = 0;
+  while (i < arr.length) {
+    chunkedArr.push(arr.slice(i, i + len));
+    i += len;
+  }
+
+  arr.forEach((val) => {});
+
+  return chunkedArr;
+}
+
+const testArray = [1, 3, 5, 43, 78, 4, 100, 4, 52];
+const testLen = 3;
+console.log(chunkArray(testArray, testLen));
+
+// Flatten array
+function flattenArray(arrays) {
+  // Solution 1
+  // return arrays.flatMap(val => val)
+
+  // Solution 2
+  // const flatArr = [];
+  // arrays.forEach((arr) => arr.forEach((val) => flatArr.push(val)));
+  // return flatArr;
+
+  // Solution 3
+  return [].concat(...arrays);
+}
+
+const testFlattenArray = [
+  [1, 3, 4],
+  [5, 6, 7],
+  [8, 9],
+];
+console.log(flattenArray(testFlattenArray));
